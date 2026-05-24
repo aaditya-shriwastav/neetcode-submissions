489ef6e7014fb7ee379ec7499a05e6c9d92bce1b
Aaditya7480 <166220985+aaditya-shriwastav@users.noreply.github.com>
2026-05-24 12:41:42 +0530

    Add: remove-duplicates-from-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array-ii/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l = 0;
+        for (int num : nums) {
+            if (l < 2 || num != nums[l - 2]) {
+                nums[l] = num;
+                l++;
+            }
+        }
+        return l;
+    }
+};
