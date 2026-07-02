b97096a882d2577c6f26dce9203b6ecbc7db2abf
Aaditya7480 <166220985+aaditya-shriwastav@users.noreply.github.com>
2026-07-02 08:48:47 +0530

    Add: candy - submission-1

---
+++ Data Structures & Algorithms/candy/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>store(n,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                if(store[i]<=store[i-1])
+                store[i]=store[i-1]+1;
+
+
+            }
+        }
+           for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                if(store[i]<=store[i+1])
+                store[i]=store[i+1]+1;
+                
+            }
+        }
+        int ans=0;
+    
+
+        for(int i=0;i<n;i++){
+ans+=store[i];
+
+        }
+
+        return ans;
+
+        
+    }
+};
